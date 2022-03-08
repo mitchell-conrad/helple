cb4d338abbd38fcc3118b31e438a3c4379cab451
Mitchell Conrad <mitchell.w.conrad@gmail.com>
2022-03-08 21:12:46 +1100

    Move to SPA for latency reasons (#10)

--- lib/wordle_companion_web/live/counter.ex
+++ lib/wordle_companion_web/live/counter.ex
@@ -9,8 +9,20 @@ defmodule WordleCompanionWeb.Counter do
        guesses: ["", "", "", "", "", ""],
        remaining: [0, 0, 0, 0, 0, 0],
        calc_time: 0,
-       remaining_words: []
+       remaining_words: [],
+       guess_histogram: ["", "", "", "", "", "", ""],
+       mean: 0,
+       std_dev: 0,
+       state: :remaining
      )}
+  end
+
+  def handle_event("mode-remaining", session, socket) do
+    {:noreply, assign(socket, state: :remaining)}
+  end
+
+  def handle_event("mode-stats", session, socket) do
+    {:noreply, assign(socket, state: :stats)}
   end
 
   def handle_event("eval-guesses", session, socket) do
@@ -31,12 +43,12 @@ defmodule WordleCompanionWeb.Counter do
     )
 
     {:noreply,
-     assign(socket, %{
+     assign(socket,
        solution: solution,
        guesses: guesses,
        remaining: nums,
        calc_time: time
-     })}
+     )}
   end
 
   def handle_event("get-words", session, socket) do
@@ -66,7 +78,45 @@ defmodule WordleCompanionWeb.Counter do
      })}
   end
 
+  def handle_event("eval-stats", session, socket) do
+    guess_fields =
+      Enum.map(
+        0..5,
+        fn x -> session["h" <> Integer.to_string(x)] end
+      )
+
+    guess_histogram =
+      Enum.map(guess_fields, fn f ->
+        case Integer.parse(f) do
+          {v, _} -> v
+          :error -> 0
+        end
+      end)
+
+    guess_histogram = [0 | guess_histogram]
+
+    mean =
+      WordleCompanion.RustSolver.external_mean(guess_histogram)
+      |> Float.round(3)
+
+    std_dev =
+      WordleCompanion.RustSolver.external_std_dev(guess_histogram)
+      |> Float.round(3)
+
+    Logger.info(mean: mean, std_dev: std_dev)
+
+    {:noreply,
+     assign(socket,
+       mean: mean,
+       std_dev: std_dev,
+       guess_histogram: guess_histogram
+     )}
+  end
+
   def render(assigns) do
-    render(WordleCompanionWeb.PageView, "index.html", assigns)
+    case assigns[:state] do
+      :remaining -> render(WordleCompanionWeb.PageView, "index.html", assigns)
+      :stats -> render(WordleCompanionWeb.PageView, "stats.html", assigns)
+    end
   end
 end
